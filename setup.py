624d0a65b8a8375217bb12a40c150058821c8d94
Alan Franzoni <username@franzoni.eu>
2011-01-11 15:06:23 +0100

    Enh: distribute/zc.buildout related files and config.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "openxenmanager",
-    install_requires = ["pygtk", "pydenji"],
+    install_requires = ["pydenji"],
     version = "0.1.0", # if possible try keeping sync with spectemplate file
     packages = find_packages(),
     author = "A. G. Rodriguez",
