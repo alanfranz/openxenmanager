cf85dc3504083234a67b3788ec02b73659e48d54
Alan Franzoni <username@franzoni.eu>
2011-01-11 15:22:20 +0100

    Enh: setuptools dep.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "openxenmanager",
-    install_requires = ["pydenji"],
+    install_requires = ["pydenji", "setuptools"],
     version = "0.1.0", # if possible try keeping sync with spectemplate file
     packages = find_packages(),
     author = "A. G. Rodriguez",
